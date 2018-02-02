bb44e43ffc121f1d9f2f3674ec093ba358d43ade
vagrant <vagrant@homestead>
2018-02-02 07:03:54 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,11 +5,18 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
     <script src="js/jquery-3.2.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-@yield('content')
+    @include('layouts._header')
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+        </div>
+    </div>
+
 </body>
 </html>
