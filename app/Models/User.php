6579376b231008e7df2ac40c4c94a96f99b6ceaf
vagrant <vagrant@homestead>
2018-02-03 02:24:21 +0000

    修改model的命名空间

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,8 +10,14 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * 指明要进行数据库交互的数据表名称
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
      * The attributes that are mass assignable.
-     *
+     *过滤用户提交的字段，只有包含在该属性中的字段才能够被正常更新
      * @var array
      */
     protected $fillable = [
@@ -20,7 +26,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     *当我们需要对用户密码或其他敏感信息在用户实例通过数组或JSON显示时进行隐藏，则可使用hidden属性
      * @var array
      */
     protected $hidden = [
